3241ea77dcd7c35624320401626d0b9023111003
William Borges <borgesw14@gmail.com>
2022-05-20 20:36:38 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  let items = [];
+  axios.get('https://pokeapi.co/api/v2/item/').then((response) => {
+    let endpoints = [];
+    response.data.results.forEach((element) => {
+      endpoints.push(element.url);
+    });
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((data) => {
+      data.forEach((element) => {
+        const item = {
+          name: element.data.name,
+          cost: element.data.cost,
+          img: element.data.sprites.default,
+        };
+        items.push(item);
+        console.log(item);
+      });
+      res.render('pages/index', { items });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
